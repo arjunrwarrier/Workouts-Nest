d2c08b66932ce5271ffdfb1fd8f1e8788ec76fd4
Arjun R Warrier <notwarrier@gmail.com>
2022-10-01 22:44:33 +0530

    modfied workout 2 trackObjectDriver.java

--- workout-2/trackObjectDriver.java
+++ workout-2/trackObjectDriver.java
@@ -10,7 +10,10 @@ static int tracker=0;
 trackObject()
 {
 tracker++;
-System.out.println(tracker+" th Object created.");  //will diplay the count of the object at execution time.
+System.out.println(tracker+" th Object created.");  //will diplay the count of the object at execution time
+}
+void displayNumber(){
+System.out.println("\nA total of "+tracker+" objects have been created."); //to display total number of objects created.
 }
 }
 
@@ -30,6 +33,9 @@ trackObject ob4= new trackObject();
 trackObject obx= new trackObject();
 
 trackObject oby= new trackObject();
+
+//simply calling displayNumber method using last created object "oby"
+oby.displayNumber();
 
 }
 }
